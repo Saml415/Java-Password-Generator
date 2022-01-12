2647664d84d6fbf81c378b9996f45657051203a3
Sam Lloyd <bobdoe784@gmail.com>
2022-01-12 15:07:26 -0800

    Add window

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,17 @@
 package com.company;
-
+import javax.swing.*;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        JFrame window = new JFrame("Password Generator");
+
+
+
+
+
+        window.setSize(400,400); // Creates the primary size of program window
+
+        window.setLayout(null); // Sets boundaries of button for program window
+        window.setVisible(true); // Makes program visible
     }
 }
