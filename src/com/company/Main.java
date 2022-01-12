2cde509162813be274d6ddeeb08eea2dbe1f6f05
Sam Lloyd <bobdoe784@gmail.com>
2022-01-12 15:54:08 -0800

    Add label

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,19 @@
 package com.company;
 import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
         JFrame window = new JFrame("Password Generator");
+        JLabel label = new JLabel("Password Generator");
+        label.setBounds(125,50,200,30);
 
 
+
+
+
+
+        window.add(label);
 
 
 
