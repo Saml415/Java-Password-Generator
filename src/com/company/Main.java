47c47fe1d90c434018be3e6aebd703c8874e10b5
Sam Lloyd <bobdoe784@gmail.com>
2022-01-12 21:45:47 -0800

    Text field and button with styles added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,22 @@
 package com.company;
 import javax.swing.*;
+import  java.awt.Color;
+import java.awt.FlowLayout;
 
 public class Main {
 
     public static void main(String[] args) {
         JFrame window = new JFrame("Password Generator");
         JLabel label = new JLabel("Password Generator");
+        JTextField  passwordBox = new  JTextField();
+        JButton submitButton = new JButton("Generate Password");
         label.setBounds(125,50,200,30);
+        passwordBox.setBounds(85,100,200,50);
+        submitButton.setBounds(105,180,160,30);
+        submitButton.setBorderPainted(false);
+        submitButton.setFocusPainted(false);
+        submitButton.setForeground(Color.white);
+        submitButton.setBackground(Color.red);
 
 
 
@@ -14,6 +24,8 @@ public class Main {
 
 
         window.add(label);
+        window.add(passwordBox);
+        window.add(submitButton);
 
 
 
